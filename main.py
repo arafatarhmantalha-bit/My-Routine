48915fcc91e69880ce339bcfeabbd47fb2bd1bce
arafatarhmantalha-bit <arafatarhmantalha@gmail.com>
2026-03-08 21:10:56 +0600

    Update main.py

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def show_days(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
         row.append(
             InlineKeyboardButton(
-                f"Ramadan {i:02d}",
+                f"Your {i:02d}",
                 callback_data=f"day_{i}"
             )
         )
